d9c76b4f0cc55cad481838caaf48ee1429f31990
Edgard Antonorsi <edgard.antonorsi@telrad.cl>
2020-08-18 15:59:40 -0400

    Added support for JWT Authentication with `/token` endpoint that returns valid JWT to anonymous users

--- dvapi/dvapi/urls.py
+++ dvapi/dvapi/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from resources import views
 
@@ -12,4 +16,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
