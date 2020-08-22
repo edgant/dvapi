d2364b1e0aeb0ee0299278c6b1217c6d7c3117f6
Edgard Antonorsi <edgard.antonorsi@telrad.cl>
2020-08-22 14:20:19 -0400

    API now returns status 400 when trying to delete Drug with associated vaccinations.

--- dvapi/resources/views.py
+++ dvapi/resources/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.db.models import ProtectedError
 from rest_framework import authentication, viewsets
+from rest_framework.exceptions import APIException
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Drug, Vaccination
 from .serializers import DrugSerializer, VaccinationSerializer
+
+
+class DrugHasVaccinations(APIException):
+    status_code = 400
+    default_detail = 'Cannot delete. This drug has vaccinations associated.'
+    default_code = 'bad_request'
 
 
 class DrugViewSet(viewsets.ModelViewSet):
@@ -13,6 +21,12 @@ class DrugViewSet(viewsets.ModelViewSet):
     queryset = Drug.objects.all()
     serializer_class = DrugSerializer
     permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, pk=None):
+        try:
+            return super().destroy(request, pk)
+        except ProtectedError:
+            raise DrugHasVaccinations
 
 
 class VaccinationViewSet(viewsets.ModelViewSet):
